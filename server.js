1e593d711044d260bc71ce8d7a3479e42c383eeb
siarjusd <siraj.haq@uniquesoftwaredev.com>
2018-06-03 12:04:43 +0500

    basic routes

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 
 const express = require('express');
 const app = express();
-const router = express.Router();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/todo', (req, res, next) => {
+  res.send('todo get');
+});
+
+app.post('/todo', (req, res, next) => {
+  res.send('todo post');
+});
+
+app.put('/todo', (req, res, next) => {
+  res.send('todo put');
+});
+
+app.delete('/:id', (req, res, next) => {
+  res.send('todo delete');
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
