47238ce22633e8f33af2acc4c17d8546df29b0dc
siarjusd <siraj.haq@uniquesoftwaredev.com>
2018-06-03 12:40:46 +0500

    todo crud implemented

--- server.js
+++ server.js
@@ -2,24 +2,54 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const _ = require('lodash')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/todo', (req, res, next) => {
-  res.send('todo get');
+let todos = [];
+let id = 0;
+
+app.get('/todo', (req, res) => {
+  res.json(todos);
 });
 
-app.post('/todo', (req, res, next) => {
-  res.send('todo post');
+app.get('/todo/:id', (req, res) => {
+  let todo = _.find(todos, { id: req.params.id });
+  res.json(todo || {});
 });
 
-app.put('/todo', (req, res, next) => {
-  res.send('todo put');
+app.post('/todo', (req, res) => {
+  let todo = req.body;
+  id++;
+  todo.id = id + '';
+  todos.push(todo);
+  res.json(todo);
 });
 
-app.delete('/:id', (req, res, next) => {
-  res.send('todo delete');
+app.put('/todo/:id', (req, res) => {
+  let update = req.body;
+  let todoIndex = _.findIndex(todos, { id: req.params.id });
+  if(!todos[todoIndex]) {
+    res.json({});
+  } else {
+    /** assign will extend current todo item with posted update
+     *  assign extend from right to left 
+     * */
+    let updatedTodo = _.assign(todos[todoIndex], update);
+    res.json(updatedTodo);
+  }
+});
+
+app.delete('/todo/:id', (req, res) => {
+  let todoIndex = _.findIndex(todos, { id: req.params.id });
+  if (!todos[todoIndex]) {
+    res.json({});
+  } else {
+    let todo = todos[todoIndex];
+    todos.splice(todoIndex, 1);
+    res.json(todo);
+  }
 });
 
 app.listen(3000, () => {
